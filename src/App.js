f80c8bfb3fb5fe38535830d835a208d60506971d
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2021-04-26 23:23:33 +0300

    Create a car shop front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,139 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Snackbar from '@material-ui/core/Snackbar';
+import AddCar from './components/AddCar';
+import EditCar from './components/EditCar';
+
 import './App.css';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 function App() {
+  const [cars, setCars] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState('');
+
+  const openSnackbar = () => {
+    setOpen(true);
+  }
+
+  const closeSnackbar = () => {
+    setOpen(false);
+  }
+
+  useEffect(() => {
+    fetchCars();
+  }, []);
+
+  const deleteCar = (url) => {
+    if (window.confirm('Are you sure?')) {
+      fetch(url, { method: 'DELETE' })
+      .then(response => {
+        if (response.ok) {
+          setMsg('Car deleted succesfully!')
+          openSnackbar();
+          fetchCars();
+        } else {
+          alert('Something went wrong!')
+        }
+      })
+      .catch(err => console.error(err))
+    }
+  }
+
+  const fetchCars = () => {
+    fetch('https://carstockrest.herokuapp.com/cars')
+    .then(response => response.json())
+    .then(data => setCars(data._embedded.cars))
+    .catch(err => console.error(err))
+  }
+
+  const addCar = (newCar) => {
+      fetch('https://carstockrest.herokuapp.com/cars', {
+        method: 'POST',
+        body: JSON.stringify(newCar),
+        headers: { 'Content-type' : 'application/json' }
+      })
+      .then(response => {
+        if (response.ok) {
+          fetchCars()
+        } else {
+          alert('Something went wrong!')
+        }
+      })
+      .catch(err => console.error(err))
+  }
+
+  const editCar = (url, updatedCar) => {
+    fetch(url, {
+      method: 'PUT',
+      body: JSON.stringify(updatedCar),
+      headers: { 'Content-type' : 'application/json' }
+    })
+    .then(response => {
+      if (response.ok)
+        fetchCars();
+      else
+        alert('Something went wrong!')
+    })
+    .catch(err => console.error(err))
+  }
+
+  const columns = [
+    { field: 'brand', sortable: true, filter: true },
+    { field: 'model', sortable: true, filter: true },
+    { field: 'color', sortable: true, filter: true },
+    { field: 'fuel', sortable: true, filter: true },
+    { field: 'year', sortable: true, filter: true, width: 100 },
+    { field: 'price', sortable: true, filter: true, width: 120 },
+    {
+      headerName: '',
+      field: '_links.self.href',
+      width: 100,
+      cellRendererFramework: params =>
+        <EditCar link={params.value} car={params.data} editCar={editCar} />
+    },
+    { 
+      headerName: '', 
+      field: '_links.self.href',
+      width: 100,
+      cellRendererFramework: params => 
+        <IconButton color="secondary" onClick={() => deleteCar(params.value)}>
+          <DeleteIcon />
+        </IconButton>
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            CarShop
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <AddCar addCar={addCar} />
+      <div className="ag-theme-material" style={{ height: 600, width: '90%', margin: 'auto' }}>
+        <AgGridReact
+          rowData={cars}
+          columnDefs={columns}
+          pagination={true}
+          paginationPageSize={10}
+          suppressCellSelection={true}
+        />
+      </div>
+      <Snackbar 
+        open={open}
+        message={msg}
+        autoHideDuration={3000}
+        onClose={closeSnackbar}  
+      />
     </div>
   );
 }
